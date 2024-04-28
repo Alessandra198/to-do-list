4337f40f343a4efa557b0e1503a6a51b815c7155
Alessandra198 <rinaldoalessandra@gmail.com>
2024-04-28 18:27:45 +0200

    Imported Bootstrap

--- src/Shopping.js
+++ src/Shopping.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Shopping() {
-  return <div>Hello from Shopping🛒</div>;
+  return (
+    <div className="Shopping">
+      <h3>Here's your Grocery Shopping list🛒😉</h3>
+      <form>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Apples
+          </label>
+        </div>
+      </form>
+    </div>
+  );
 }
