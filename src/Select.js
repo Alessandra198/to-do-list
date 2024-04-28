a0425d424dd84ad6aa3896ef9cf20f576c6eac9d
Alessandra198 <rinaldoalessandra@gmail.com>
2024-04-28 17:39:39 +0200

    Added Choose.js component

--- src/Select.js
+++ src/Select.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import Choose from "./Choose";
 
 export default function Select() {
+  let [value, setValue] = useState(null);
+
+  function handleChange(event) {
+    event.preventDefault();
+    setValue(event.target.value);
+  }
   return (
     <div className="Select">
       <h2>Which list do you need?</h2>
-      <select>
-        <option value="">Choose one list😉</option>
-        <option value="shopping">Grocery Shopping</option>
-        <option value="cleaning">Home Cleaning</option>
-        <option value="morning">Morning Routine</option>
-        <option value="night">Night Routine</option>
-      </select>
+      <div className="selection">
+        <select onChange={handleChange}>
+          <option value="">Choose one list😉</option>
+          <option value="shopping">Grocery Shopping🛒</option>
+          <option value="cleaning">Home Cleaning🧽</option>
+          <option value="morning">Morning Routine🌞</option>
+          <option value="night">Night Routine🌛</option>
+        </select>
+      </div>
+      <Choose value={value} />
     </div>
   );
 }
