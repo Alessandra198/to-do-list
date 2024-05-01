3a3b2b3dd5da5c515f04b4ab176fe528d81d0804
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-01 17:54:35 +0200

    Added Morning Routine

--- src/Morning.js
+++ src/Morning.js
@@ -4,6 +4,60 @@ export default function Morning() {
   return (
     <div>
       <h3>Here's your Morning Routine CheckList🌞😉</h3>
+      <div>
+        <label>
+          <input type="checkbox" />
+          Dental Floss
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" />
+          Brush your teeth
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" />
+          Mouthwash
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" />
+          Wash your face
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" />
+          Tonic
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" />
+          Sierum
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" />
+          Eye Cream
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" />
+          Moisturizing cream
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" />
+          SPF cream
+        </label>
+      </div>
     </div>
   );
 }
