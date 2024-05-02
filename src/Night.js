71b239e64df6bc36e9d5a72a11d3f0c3802309b0
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-02 15:07:17 +0200

    Fixed style

--- src/Night.js
+++ src/Night.js
@@ -2,79 +2,81 @@ import React from "react";
 
 export default function Morning() {
   return (
-    <div>
-      <h3>Here's your Morning Routine CheckList🌞😉</h3>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Take a shower
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Dental Floss
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Brush your teeth
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Mouthwash
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Wash your face
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Tonic
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Sierum
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Eye Cream
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Moisturizing cream
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Night mask
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Protective hairstyle
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" />
-          Sweet Dreams🌛
-        </label>
+    <div className="Morning">
+      <div className="check">
+        <h3>Here's your Morning Routine CheckList🌞😉</h3>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Take a shower
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Dental Floss
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Brush your teeth
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Mouthwash
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Wash your face
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Tonic
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Sierum
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Eye Cream
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Moisturizing cream
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Night mask
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Protective hairstyle
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" />
+            Sweet Dreams🌛
+          </label>
+        </div>
       </div>
     </div>
   );
